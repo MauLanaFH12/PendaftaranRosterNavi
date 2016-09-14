dc882aee56bc9233361f58f0a9f9b68c7f8f9779
Maulana Fadia Hadi <maulanafadia118@gmail.com>
2016-09-14 13:31:25 +0700

    penambahan fungsi checkbox dan radiobutton

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4020/myapps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4020/myapps/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button bRegister;
     CheckBox cbM, cbC, cbO, cbS4, cbS5;
     Spinner Negara;
-    TextView tvView, tvView2, tvView3, tvView4;
+    TextView tvView, tvViewR, tvViewC, tvViewT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +34,10 @@ public class MainActivity extends AppCompatActivity {
         cbO = (CheckBox) findViewById(R.id.checkBoxO);
         cbS4 = (CheckBox) findViewById(R.id.checkBoxS4);
         cbS5 = (CheckBox) findViewById(R.id.checkBoxS5);
-        tvView = (TextView) findViewById(R.id.textView3);
-        tvView2 = (TextView) findViewById(R.id.textView7);
-        tvView3 = (TextView) findViewById(R.id.textView8);
-        tvView4 = (TextView) findViewById(R.id.textView9);
+        tvViewT = (TextView) findViewById(R.id.textViewHN);
+        tvView = (TextView) findViewById(R.id.textViewHA);
+        tvViewC = (TextView) findViewById(R.id.textViewHR);
+        tvViewR = (TextView) findViewById(R.id.textViewHG);
         Negara = (Spinner) findViewById(R.id.spinnerProvinsi);
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,14 +51,71 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 doClick();
+                doClick2();
+                doClick3();
             }
         });
     }
 
     private void doProcess() {
+        if (isTrue()) {
+            String name = etName.getText().toString();
+            tvViewT.setText(name + ", ");
+
+        }
     }
 
+
     private void doClick() {
-        tvView4.setText("Your State :" + Negara.getSelectedItem().toString());
+        tvView.setText("Your State :" + Negara.getSelectedItem().toString());
+    }
+
+    private void doClick2() {
+        String hasilC = "Your Role : \n";
+        int startlen = hasilC.length();
+        if (cbM.isChecked()) hasilC += cbM.getText() + "\n";
+        if (cbC.isChecked()) hasilC += cbC.getText() + "\n";
+        if (cbO.isChecked()) hasilC += cbO.getText() + "\n";
+        if (cbS4.isChecked()) hasilC += cbS4.getText() + "\n";
+        if (cbS5.isChecked()) hasilC += cbS5.getText() + "\n";
+        if (tvView.length() == startlen) hasilC += "No Role Picked!";
+        tvViewC.setText("" + hasilC.toString());
+    }
+
+    private void doClick3() {
+        String hasilR = null;
+        if (rbD.isChecked()) {
+            hasilR = rbD.getText().toString();
+        }
+        if (rbL.isChecked()) {
+            hasilR = rbL.getText().toString();
+        }
+        if (rbH.isChecked()) {
+            hasilR = rbH.getText().toString();
+        }
+        if (rbC.isChecked()) {
+            hasilR = rbC.getText().toString();
+        }
+        if (tvViewR == null) {
+            tvViewR.setText("You must choose your Games !");
+        } else {
+            tvViewR.setText("Your Role : " + hasilR);
+        }
+    }
+
+    private boolean isTrue() {
+        boolean valid = true;
+        String name = etName.getText().toString();
+
+        if (name.isEmpty()) {
+            etName.setError("You Should Insert Your Names !");
+            valid = false;
+        } else if (name.length() < 4) {
+            etName.setError("Your Name Must Be at Least 4 Characters");
+            valid = false;
+        } else {
+            etName.setError(null);
+        }
+        return valid;
     }
 }
